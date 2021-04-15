1228860b20ae20e326ad5cb7aa1b9347c9c5ae2d
Matheus-mVilela <matheus_vilela@icloud.com>
2021-04-15 06:38:17 -0300

    Fibonacci

---
+++ pacote /interavel.py
@@ -0,0 +1,23 @@
+# Fibonaci
+
+
+class Fibonaci:
+    def __init__(self, max):
+        self.max = max
+
+    def __iter__(self):
+        self.x, self.y = 1, 1
+        return self
+
+    def __next__(self):
+        fib = self.x
+        if fib > self.max:
+            raise StopIteration
+        self.x, self.y = self.y, self.x + self.y
+        return fib
+
+
+fib = Fibonaci(100)
+for i in fib:
+    print(i)
+

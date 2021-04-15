f1eb10f2df50339aa088b51be06d2d039cc4b10a
Matheus-mVilela <matheus_vilela@icloud.com>
2021-04-15 05:39:55 -0300

    dicionario

---
+++ pacote /dicionarios.py
@@ -0,0 +1,14 @@
+d = {'matheus': 22, 'jayce': 25}
+print(d)
+
+print(d.get('matheus'))
+
+print(d.keys())
+
+print(d.values())
+
+print(d.items())
+
+for nome, idade in d.items():
+    print(nome, idade)
+
